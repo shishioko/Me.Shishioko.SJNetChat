2b25ed2b042ad826691e687e7a076df5feb1e38c
shishioko <shishioko@proton.me>
2024-08-05 08:39:13 +0200

    bugfix

--- Extensions/SJNCNameExtension.cs
+++ Extensions/SJNCNameExtension.cs
@@ -9,7 +9,8 @@ namespace Me.Shishioko.SJNetChat.Extensions
     public sealed class SJNCNameExtension : SJNCExtension
     {
         public string? Name { get; private set; }
-        public string? LatestReceivedName { get; private set; } = null;
+        public string? AttachedName { get; private set; } = null;
+        private bool Server = false;
         public SJNCNameExtension(string? name)
         {
             if (name is not null) Contract.Assert(name.Length <= 32);
@@ -22,6 +23,7 @@ namespace Me.Shishioko.SJNetChat.Extensions
         protected internal override async Task<Stream> OnInitializeAsync(Stream stream, bool server)
         {
             Contract.Assert(server != Name is not null);
+            Server = server;
             if (server) Name = await stream.ReadStringAsync(SizePrefix.U8, byte.MaxValue);
             else await stream.WriteStringAsync(Name!, SizePrefix.U8, byte.MaxValue);
             if (Name.Length > 32) throw new ProtocolViolationException("Name longer than 32 characters!");
@@ -29,13 +31,15 @@ namespace Me.Shishioko.SJNetChat.Extensions
         }
         protected internal override async Task OnTextSendAsync(Stream stream, string message)
         {
-            Contract.Assert(Name.Length <= 32);
-            await stream.WriteStringAsync(Name!, SizePrefix.U8, byte.MaxValue);
+            if (!Server) return;
+            Contract.Assert(AttachedName.Length <= 32);
+            await stream.WriteStringAsync(AttachedName!, SizePrefix.U8, byte.MaxValue);
         }
         protected internal override async Task OnTextReceiveAsync(Stream stream, string message)
         {
-            LatestReceivedName = await stream.ReadStringAsync(SizePrefix.U8, byte.MaxValue);
-            if (LatestReceivedName.Length > 32) throw new ProtocolViolationException("Name longer than 32 characters!");
+            if (Server) return;
+            AttachedName = await stream.ReadStringAsync(SizePrefix.U8, byte.MaxValue);
+            if (AttachedName.Length > 32) throw new ProtocolViolationException("Name longer than 32 characters!");
         }
         protected internal override Task OnReceiveAsync(byte[] data)
         {
