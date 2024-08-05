cac3e484198079fa17d4f8aef9a99b461be894b4
shishioko <shishioko@proton.me>
2024-08-05 06:24:13 +0200

    Add project files.

---
+++ Me.Shishioko.SJNetChat.Client/SJNCExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.Contracts;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Me.Shishioko.SJNetChat
+{
+    public abstract class SJNCExtension
+    {
+        internal SJNC? SJNC = null;
+        internal bool Unlocked = false;
+        internal ushort Channel = 0;
+        internal readonly SemaphoreSlim Sync = new(1, 1);
+        public SJNCExtension()
+        {
+
+        }
+        internal protected abstract string Identify();
+        internal protected abstract Task<Stream> OnInitializeAsync(Stream stream, bool server);
+        internal protected abstract Task OnReceiveAsync(byte[] data);
+        protected Task SendAsync(byte[] data)
+        {
+            Contract.Assert(Unlocked);
+            return SJNC!.SendAsync(Channel, data);
+        }
+    }
+}
---
+++ Me.Shishioko.SJNetChat.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Me.Shishioko.SJNetChat.Test
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("[ server ] [ client ]");
+            string? option = Console.ReadLine();
+            if (option == "server") await ExampleServer.Main();
+            else if (option == "client") await ExampleClient.Main();
+            else Console.WriteLine("Unknown option!");
+        }
+    }
+}
