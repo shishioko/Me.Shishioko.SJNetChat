c7443325f9dff2aa742df1833486242c5acd89b5
Jaeger <90076904+TheGreatJaeger@users.noreply.github.com>
2024-10-09 20:33:24 -0500

    Add ShioConsole and Jaeger's BotClient example
    Added ShioConsole Async console printing and Jaeger's BotClient example

---
+++ Examples/BotClient.cs
@@ -0,0 +1,89 @@
+﻿using Me.Shishioko.SJNetChat;
+using Me.Shishioko.SJNetChat.Extensions;
+using System.Drawing;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace SJNCCLI
+{
+
+    public static class Program
+    {
+        private static string LocalName = "User";
+        //public static void Main(string[] arguments)
+        public static void Main(string[] arguments)
+        {
+            Stopwatch uptime = Stopwatch.StartNew();
+            string encryption = "True";
+
+            //if (arguments.Length < 2)
+            //{
+            //ShioConsole.WriteLine("Usage: sjnccli.exe <endpoint> <name> [-noencrypt]");
+            //return;
+            //}
+
+            //string[] parameters = arguments.Skip(2).ToArray();
+            Console.Write("Enter Username: ");
+            string LocalName = Console.ReadLine();
+            Console.Write("Enter Host URL: ");
+            string HostUrl = Console.ReadLine();
+            Console.Clear();
+            SJNCNameExtension name = new(LocalName); // = arguments[1]
+            string fullAddress = HostUrl;
+            int fullAddressSplit = fullAddress.LastIndexOf(':');
+            using SJNC sjnc = SJNC.Connect(new DnsEndPoint(fullAddress[..fullAddressSplit], ushort.Parse(fullAddress[(fullAddressSplit + 1)..])));
+            SJNCAesExtension? aes = new();
+            SJNCOnlineNameListExtension nameList = new(null);
+            List<string> list = [];
+            nameList.OnAddAsync += async (name) => {
+                list.Add(name);
+            };
+            nameList.OnRemoveAsync += async (name) =>
+            {
+                list.Remove(name);
+            };
+            List<SJNCExtension> extensions = [name, nameList, aes];
+            sjnc.Initialize(extensions);
+            foreach (SJNCExtension extension in extensions.Where(extension => !extension.Common)) ShioConsole.WriteLine($"{ComputeColor(LocalName, 32)}Extension {ComputeColor(LocalName, 64)}{extension.GetType().Name} {ComputeColor(LocalName, 32)}was not loaded!");
+            ThreadPool.QueueUserWorkItem((object? _) =>
+            {
+                while (true)
+                {
+                    string message = sjnc.Receive();
+                    string sender = name.AttachedName ?? "User";
+
+                    ShioConsole.WriteLine($"{ComputeColor(sender, 64)}{sender}{ComputeColor(sender, 128)}: {message}");
+                }
+            });
+            while (true)
+            {
+                string input = ShioConsole.ReadLine(input => $"{ComputeColor(LocalName, 128)}{input}");
+                if (input == "?uptime")
+                {
+                    ShioConsole.WriteLine($"Program Uptime: {uptime.Elapsed}");
+                    continue;
+                };
+                if (input == "?online")
+                {
+                    string users = string.Join(", ", list);
+                    ShioConsole.WriteLine($"Users online: {users}");
+                    continue;
+                };
+                sjnc.Send(input);
+            }
+        }
+        private static string ComputeColor(string input, int shift)
+        {
+            byte[] hash = MD5.HashData(Encoding.UTF8.GetBytes(input));
+            Color color = Color.FromArgb(hash[0] / 2 + shift, hash[1] / 2 + shift, hash[2] / 2 + shift);
+            return $"\u001b[38;2;{color.R};{color.G};{color.B}m";
+        }
+    }
+}
---
+++ Examples/ShioConsole.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+
+namespace SJNCCLI
+{
+    public static class ShioConsole
+    {
+        private static readonly SemaphoreSlim WriteSync = new (1, 1);
+        private static readonly SemaphoreSlim ReadSync = new (1, 1);
+        private static string? InputVisual = null;
+        static ShioConsole()
+        {
+            Console.OutputEncoding = Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("\u001b[s");
+        }
+        public static void Write(string text)
+        {
+            WriteSync.Wait();
+            Console.Write($"\u001b[2K\u001b[u{text}\u001b[s\u001b[1E{InputVisual}");
+            WriteSync.Release();
+        }
+        public static void WriteLine(string text)
+        {
+            Write($"{text}\n");
+        }
+        public static string ReadLine(bool silent = false)
+        {
+            return ReadLine(input => input, silent);
+        }
+        public static string ReadLine(Func<string, string> transformer, bool silent = false)
+        {
+            int cursor = 0;
+            string input = string.Empty;
+            ReadSync.Wait();
+            try
+            {
+                while (true)
+                {
+                    if (!silent) Console.Write($"\u001b[?25l\u001b[2K\u001b[0G{InputVisual = transformer(input)}\u001b[{cursor + 1}G\u001b[?25h");
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    if (!silent) WriteSync.Wait();
+                    try
+                    {
+                        if (char.IsControl(key.KeyChar))
+                        {
+                            if (key.Key == ConsoleKey.Enter)
+                            {
+                                if (!silent) Console.Write($"\u001b[2K\u001b[0G");
+                                return input;
+                            }
+                            if (key.Key == ConsoleKey.Backspace)
+                            {
+                                int erase = int.Min(cursor, key.Modifiers.HasFlag(ConsoleModifiers.Control) ? int.Max(1, cursor - (input[..cursor].LastIndexOf(' ') + 1)) : 1);
+                                input = input[..(cursor - erase)] + input[cursor..];
+                                cursor -= erase;
+                            }
+                            if (key.Key == ConsoleKey.Delete)
+                            {
+                                input = input[..cursor] + input[(cursor == input.Length ? cursor : cursor + 1)..];
+                            }
+                            if (key.Key == ConsoleKey.LeftArrow)
+                            {
+                                cursor = int.Clamp(cursor - 1, 0, input.Length);
+                                if (key.Modifiers.HasFlag(ConsoleModifiers.Control)) cursor = input[..cursor].LastIndexOf(' ') + 1;
+                            }
+                            if (key.Key == ConsoleKey.RightArrow)
+                            {
+                                if (key.Modifiers.HasFlag(ConsoleModifiers.Control))
+                                {
+                                    int offset = input[cursor..].IndexOf(' ');
+                                    cursor = offset < 0 ? input.Length : cursor + offset;
+                                }
+                                cursor = int.Clamp(cursor + 1, 0, input.Length);
+                            }
+                        }
+                        else
+                        {
+                            input = input.Insert(cursor++, key.KeyChar.ToString());
+                        }
+                    }
+                    finally
+                    {
+                        if (!silent) WriteSync.Release();
+                    }
+                }
+            }
+            finally
+            {
+                InputVisual = null;
+                ReadSync.Release();
+            }
+        }
+    }
+}
